45c9b0ce82acf19393b5a1945de3d749ea7385d0
Lauren Eisdorfer <leisdorfer67@gmail.com>
2018-02-06 21:21:19 -0600

    Initial commit

---
+++ CodeWithACause/Source/ViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+import RxSugar
+import RxSwift
+
+class ViewController: UIViewController {
+
+    override func loadView() {
+        super.loadView()
+        view.backgroundColor = .red
+    }
+}
+
